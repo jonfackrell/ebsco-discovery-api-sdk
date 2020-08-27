14d3220984625d87c390fcd9848b2d03d9070217
Jon Fackrell <jon.fackrell@gmail.com>
2020-08-27 11:09:51 -0600

    Apply fixes from StyleCI (#2)

--- src/EbscoDiscoveryApiServiceProvider.php
+++ src/EbscoDiscoveryApiServiceProvider.php
@@ -1,8 +1,6 @@
 <?php
 
-
 namespace JonFackrell\Eds;
-
 
 use Illuminate\Support\ServiceProvider;
 
@@ -10,18 +8,16 @@ class EbscoDiscoveryApiServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-
-        if(! class_exists('CreateEdsApiTable')){
+        if (! class_exists('CreateEdsApiTable')) {
             $this->publishes([
-                __DIR__.'../database/migrations/create_eds_api_table.php.stub' => database_path('migrations/'.date('Y_m_d_His', time()).'_create_eds_api_table.php')
+                __DIR__.'../database/migrations/create_eds_api_table.php.stub' => database_path('migrations/'.date('Y_m_d_His', time()).'_create_eds_api_table.php'),
             ], 'migrations');
         }
-
     }
 
     public function register()
     {
-        $this->app->bind('eds', function (){
+        $this->app->bind('eds', function () {
             return new Eds();
         });
     }
